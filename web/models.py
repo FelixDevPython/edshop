ac96d5e71d169c1fd56381a78930382ce6bf460e
Felix Miranda <felix.miranda.sandoval@gmail.com>
2025-04-10 21:26:36 -0400

    MODULO 4

--- web/models.py
+++ web/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Categoria(models.Model):
@@ -19,4 +20,41 @@ class Producto(models.Model):
     def __str__(self):
         return self.nombre
     
+class Cliente(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.RESTRICT)
+    dni= models.CharField(max_length=8)
+    sexo = models.CharField(max_length=1, default='M')
+    telefono = models.CharField(max_length=20)
+    fecha_nacimiento = models.DateField(null=True)
+    direccion = models.TextField()
+
+    def __str__(self):
+        return self.dni
+    
+class Pedido(models.Model):
+
+    ESTADO_CHOICES = (
+        ('0', 'Solicitado'),
+        ('1', 'Pagado'),
+    )
+
+    cliente = models.ForeignKey(Cliente, on_delete=models.RESTRICT)
+    # RESTRICT: No se puede eliminar un cliente si tiene pedidos
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    nro_pedido = models.CharField(max_length=20, null=True)
+    monto_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    estado = models.CharField(max_length=20, default='0', choices=ESTADO_CHOICES)
+    # 0: Solicitado, 1: Pagado
+
+    def __str__(self):
+        return self.nro_pedido
+    
+class PedidoDetalle(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.RESTRICT)
+    producto = models.ForeignKey(Producto, on_delete=models.RESTRICT)
+    cantidad = models.IntegerField(default=1)
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
+    # Se usa subtotal porque el precio puede cambiar, en caso de que no almacenaras el costo que tuvo el producto al momento de la venta, cuando se cambie el precio, se va a perder ese dato, de esta manera se registra a cuanto se vendió el producto, si quiero saber el precio unitario en el momento de la venta, lo puedo obtener dividiendo el subtotal entre la cantidad
+    def __str__(self):
+        return self.producto.nombre
     
--- web/views.py
+++ web/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render, get_object_or_404
-
-from .models import Categoria,Producto
+from django.shortcuts import render, get_object_or_404,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+# Los decoradores son funciones que se utilizan para modificar el comportamiento de una función
+# El decorador login_required lo que va a hecer es antes de ejecutar la funcion, va a verificar si el usuario esta logueado dejando entrar a la funcion pero si no lo esta, lo redirige a la pagina de login
+from .models import Categoria,Producto,Cliente
+from .carrito import Cart
+from .forms import ClienteForm
 
 # Create your views here.
 """ VISTAS PARA MOSTRAR EL CATALOGO DE PRODUCTOS """
@@ -50,3 +56,183 @@ def productoDetalle(request, producto_id):
     }
 
     return render(request, 'producto.html',context)
+
+""" vistas para el carrito de compras """
+def carrito(request):
+    return render(request, 'carrito.html')
+
+def agregarCarrito(request, producto_id):
+    if request.method == 'POST':
+        cantidad = int(request.POST['cantidad'])
+    else:
+        cantidad = 1
+
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.add(objProducto, cantidad)
+
+    #print(request.session.get('cart'))
+
+    if request.method == 'GET':
+        return redirect('/')
+
+    return render(request, 'carrito.html')
+
+def eliminarProductoCarrito(request, producto_id):
+    objProducto = Producto.objects.get(pk=producto_id)
+    carritoProducto = Cart(request)
+    carritoProducto.delete(objProducto)
+
+    return render(request, 'carrito.html')
+
+def limpiarCarrito(request):
+    carritoProducto = Cart(request)
+    carritoProducto.clear()
+    return render(request, 'carrito.html')
+
+
+""" VISTAS PARA CLIENTES Y USUARIOS """
+def crearUsuario(request):
+
+    if request.method == 'POST':
+        dataUsuario = request.POST['nuevoUsuario']
+        dataPassword = request.POST['nuevoPassword']
+
+        nuevoUsuario = User.objects.create_user(username=dataUsuario, password=dataPassword)
+        if nuevoUsuario is not None:
+            login(request, nuevoUsuario)
+            return redirect('/cuenta')
+
+    return render(request, 'login.html')
+
+def loginUsuario(request):
+    paginaDestino = request.GET.get('next')
+    # Se obtiene la pagina a la que se quiere redirigir al usuario una vez logueado
+    context = {
+        'destino': paginaDestino,
+    }
+
+    if request.method == 'POST':
+        dataUsuario = request.POST['usuario']
+        dataPassword = request.POST['password']
+        dataDestino = request.POST['destino']
+        # Se obtiene el destino al que se quiere redirigir al usuario una vez logueado
+
+        usuarioAuth = authenticate(request,username=dataUsuario, password=dataPassword)
+        if usuarioAuth is not None:
+            login(request, usuarioAuth)
+
+            if dataDestino != 'None':
+                # Si el destino no es None, se redirige a la pagina destino
+                return redirect(dataDestino)
+
+            return redirect('/cuenta')
+        else:
+            context = {
+                'mensajeError':'Datos incorrectos',
+            }
+
+    return render(request, 'login.html', context)
+
+def logoutUsuario(request):
+    logout(request)
+    return render(request,'login.html')
+
+def cuentaUsuario(request):
+    try:
+        clienteEditar = Cliente.objects.get(usuario=request.user)
+        # Se obtiene el usuario que se logueo, como parametro a la variable usuario y va a obtener el cliente
+        dataCliente = {
+            # Estos datos vienen del usuario que se logueo, que esta dentro de la variable request, que se crea al loguearse
+            # Estos datos vienen de la tabla usuario
+            'nombre':request.user.first_name,
+            'apellidos':request.user.last_name,
+            'email':request.user.email,
+            # Estos datos vienen de la tabla cliente
+            'direccion':clienteEditar.direccion,
+            'telefono':clienteEditar.telefono,
+            'dni':clienteEditar.dni,
+            'sexo':clienteEditar.sexo,
+            'fecha_nacimiento':clienteEditar.fecha_nacimiento,
+        }
+    except:
+        dataCliente = {
+            'nombre':request.user.first_name,
+            'apellidos':request.user.last_name,
+            'email':request.user.email,
+        }
+        # Si no existe el cliente, se crea un diccionario con los datos del usuario logueado, los datos vienen desde la tabla usuario
+
+    frmCliente = ClienteForm(dataCliente)
+    # Se crea una instancia del formulario ClienteForm, y se le pasa el diccionario dataCliente
+    context = {
+        'frmCliente': frmCliente,
+    }
+
+    return render(request, 'cuenta.html', context)
+
+def actualizarCliente(request):
+    mensaje = ""
+    if request.method == 'POST':
+        frmCliente = ClienteForm(request.POST)
+        # Todo lo que se envie por POST se va a signar a la clase ClienteForm
+        if frmCliente.is_valid():
+            # El metodo is_valid() valida los datos
+            dataCliente = frmCliente.cleaned_data
+            # El metodo cleaned_data prepara la data para ser guardada en la base de datos
+            actUsuario = User.objects.get(pk=request.user.id)
+            # Se obtiene el usuario que se va a actualizar, en este caso el que esta logeado
+            actUsuario.first_name = dataCliente['nombre']
+            actUsuario.last_name = dataCliente['apellidos']
+            actUsuario.email = dataCliente['email']
+            actUsuario.save()
+            
+            # Registrar Cliente
+            nuevoCliente = Cliente()
+            nuevoCliente.usuario = actUsuario
+            nuevoCliente.dni = dataCliente['dni']
+            nuevoCliente.direccion = dataCliente['direccion']
+            nuevoCliente.telefono = dataCliente['telefono']
+            nuevoCliente.sexo = dataCliente['sexo']
+            nuevoCliente.fecha_nacimiento = dataCliente['fecha_nacimiento']
+            nuevoCliente.save()
+
+            mensaje = "Datos actualizados"
+
+    context = {
+        'mensaje': mensaje,
+        'frmCliente': frmCliente,
+    }
+
+    return render(request, 'cuenta.html', context)
+
+""" VISTAS PARA PROCESO DE COMPRA """
+@login_required(login_url='/login')
+def registrarPedido(request):
+    try:
+        clienteEditar = Cliente.objects.get(usuario=request.user)
+        dataCliente = {
+            'nombre':request.user.first_name,
+            'apellidos':request.user.last_name,
+            'email':request.user.email,
+            'direccion':clienteEditar.direccion,
+            'telefono':clienteEditar.telefono,
+            'dni':clienteEditar.dni,
+            'sexo':clienteEditar.sexo,
+            'fecha_nacimiento':clienteEditar.fecha_nacimiento,
+            }
+    except:
+        dataCliente = {
+            'nombre':request.user.first_name,
+            'apellidos':request.user.last_name,
+            'email':request.user.email,
+        }
+
+    frmCliente = ClienteForm(dataCliente)
+    context = {
+        'frmCliente': frmCliente,
+    }
+
+    return render(request, 'pedido.html', context)
+
+
